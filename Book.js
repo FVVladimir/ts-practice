aaf645f48583607b06660691e775a6d1109aa1c1
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-01 20:33:31 +0300

    creat js file

---
+++ Book.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Book = /** @class */ (function () {
+    function Book(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    ;
+    Book.prototype.getSummary = function () {
+        return "".concat(this.title, " by ").concat(this.author, "published in ").concat(this.year);
+    };
+    return Book;
+}());
+exports.default = Book;
