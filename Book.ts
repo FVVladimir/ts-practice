a5cdff27cc93f6d014b554b06b40a16a6a4ddc73
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-01 20:33:19 +0300

    create class book

---
+++ Book.ts
@@ -0,0 +1,19 @@
+class Book {
+    private title: string;
+    private author: string;
+    private year: number;
+
+    constructor(title: string, author: string, year: number){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    };
+
+   public  getSummary (){
+        return `${this.title} by ${this.author}published in ${this.year}`
+    }
+
+
+}
+
+export default Book;
